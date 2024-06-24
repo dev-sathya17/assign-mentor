95a9da30f8ad9d62607d60e1a57926c0ad60f34b
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-24 11:59:21 +0530

    server created and running

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
-const { MONGODB_URI } = require("./utils/config");
+const { MONGODB_URI, PORT } = require("./utils/config");
+const app = require("./app");
 
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    console.log("Database connected ");
+    console.log("Database connected");
+    app.listen(PORT, () => {
+      console.log("Server is running on port:", PORT);
+    });
   })
   .catch((error) => {
     console.log("An error occurred while connecting to database", error);
