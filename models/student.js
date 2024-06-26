fa7b909a859a824ef31fab7165a1b4f85652dec4
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-26 16:40:59 +0530

    added default value in schema for students model

--- models/student.js
+++ models/student.js
@@ -16,6 +16,7 @@ const studentSchema = new mongoose.Schema({
   currentMentor: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Mentor",
+    default: null,
   },
   previousMentors: {
     type: [
