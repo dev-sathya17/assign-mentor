08fd947cfa90431145346c823196c60a26e857b6
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-26 16:16:11 +0530

    models and schemas added

---
+++ models/student.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  currentMentor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Mentor",
+  },
+  previousMentors: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Mentor",
+      },
+    ],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("Student", studentSchema, "students");
